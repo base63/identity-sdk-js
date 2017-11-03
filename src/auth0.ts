fa231eae32e82096008637db359e44c89c52eaea
Horia Coman <horia141@gmail.com>
2017-11-03 17:21:40 +0200

    Move the auth0token marshaller to the flow

--- src/auth0.ts
+++ src/auth0.ts
@@ -1,26 +1,6 @@
-import { ExtractError, StringMarshaller } from 'raynor'
-
-
 export class Auth0Config {
     clientId: string;
     clientSecret: string;
     domain: string;
     callbackUri: string;
-}
-
-
-export class Auth0AccessTokenMarshaller extends StringMarshaller {
-    private static readonly _alnumRegExp: RegExp = new RegExp('^[0-9a-zA-Z_-]+$');
-
-    filter(s: string): string {
-        if (s.length == 0) {
-            throw new ExtractError('Expected a string to be non-empty');
-        }
-
-        if (!Auth0AccessTokenMarshaller._alnumRegExp.test(s)) {
-            throw new ExtractError('Should only contain alphanumerics');
-        }
-
-        return s;
-    }
 }
--- src/server/auth0-auth-flow-router.ts
+++ src/server/auth0-auth-flow-router.ts
@@ -6,7 +6,7 @@ import { ExtractError, MarshalFrom, MarshalWith, OptionalOf, StringMarshaller } 
 import { Env } from '@base63/common-js'
 import { WebFetcher } from '@base63/common-server-js'
 
-import { Auth0AccessTokenMarshaller, Auth0Config } from '../auth0'
+import { Auth0Config } from '../auth0'
 import { PostLoginRedirectInfo, PostLoginRedirectInfoMarshaller } from '../auth-flow'
 import { IdentityClient } from '../client'
 import { RequestWithIdentity } from '../request'
@@ -18,6 +18,23 @@ import {
     clearSessionTokenOnResponse
 } from './session-middleware'
 import { SessionToken } from '../session-token'
+
+
+class Auth0AccessTokenMarshaller extends StringMarshaller {
+    private static readonly _alnumRegExp: RegExp = new RegExp('^[0-9a-zA-Z_-]+$');
+
+    filter(s: string): string {
+        if (s.length == 0) {
+            throw new ExtractError('Expected a string to be non-empty');
+        }
+
+        if (!Auth0AccessTokenMarshaller._alnumRegExp.test(s)) {
+            throw new ExtractError('Should only contain alphanumerics');
+        }
+
+        return s;
+    }
+}
 
 
 class Auth0AuthorizationCodeMarshaller extends StringMarshaller {
