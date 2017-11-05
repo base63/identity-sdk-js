ce5527cbf44af9932bc5e9de993837cc65654126
Horia Coman <horia141@gmail.com>
2017-11-05 16:20:20 +0200

    Add comments for the xsrf token middleware

--- src/server/check-xsrf-token-middleware.ts
+++ src/server/check-xsrf-token-middleware.ts
@@ -1,3 +1,6 @@
+/** Defines middleware for protecting against XSRF attacks. */
+
+/** Imports. Also so typedoc works correctly. */
 import * as express from 'express'
 import * as HttpStatus from 'http-status-codes'
 
@@ -6,6 +9,13 @@ import { Session, XsrfTokenMarshaller } from '../entities'
 import { XSRF_TOKEN_HEADER_NAME } from '../client'
 
 
+/**
+ * Create an express middleware for protection against XSRF attacks, for the identity service.
+ * Needs to apply after the session middleware, since it depends on a session existing. What this
+ * does is simply check the contents of the header given by {@link XSRF_TOKEN_HEADER_NAME} against
+ * the XSRF token attached to the session. If they match the request is allowed to further stages.
+ * @return A connect middleware doing all of the above.
+ */
 export function newCheckXsrfTokenMiddleware() {
     const xsrfTokenMarshaller = new XsrfTokenMarshaller();
 
