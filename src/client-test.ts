1acc8527160c23c75340b158df243557f5a31506
Horia Coman <horia141@gmail.com>
2017-11-10 15:14:46 +0200

    Add some tests for the client module. Not enough though

--- src/client-test.ts
+++ src/client-test.ts
@@ -1,1 +1,27 @@
-import './client'
+import { expect } from 'chai'
+import 'mocha'
+
+import {
+    IdentityError,
+    UnauthorizedIdentityError
+} from './client'
+
+
+describe('IdentityError', () => {
+    it('should construct a proper error', () => {
+        const error = new IdentityError('A problem');
+        expect(error.name).to.eql('IdentityError');
+        expect(error.message).to.eql('A problem');
+        expect(error.stack).to.be.not.null;
+    });
+});
+
+
+describe('UnauthorizedIdentityError', () => {
+    it('should construct a proper error', () => {
+        const error = new UnauthorizedIdentityError('A problem');
+        expect(error.name).to.eql('UnauthorizedIdentityError');
+        expect(error.message).to.eql('A problem');
+        expect(error.stack).to.be.not.null;
+    });
+});
