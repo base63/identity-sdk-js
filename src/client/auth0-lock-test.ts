10213a465f9e01add0acc2254842c0c7763454ce
Horia Coman <horia141@gmail.com>
2017-11-21 20:50:26 +0200

    A first test for the Auth0Lock

--- src/client/auth0-lock-test.ts
+++ src/client/auth0-lock-test.ts
@@ -1,1 +1,25 @@
-import './auth0-lock'
+import { expect } from 'chai'
+import { History } from 'history'
+import 'mocha'
+import * as td from 'testdouble'
+
+import { Auth0Lock } from './auth0-lock'
+import { Auth0Config } from '../auth0'
+
+
+describe('Auth0Lock', () => {
+    const auth0Config: Auth0Config = {
+        clientId: 'some-id',
+        clientSecret: 'someSecret',
+        domain: 'the-domain',
+        loginCallbackUri: '/auth/login'
+    };
+
+    it('can be constructed', () => {
+        const history = td.object({});
+
+        const auth0Lock = new Auth0Lock(history as History, auth0Config);
+
+        expect(auth0Lock).is.not.null;
+    });
+});
