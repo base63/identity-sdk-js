b15136e41198de01527af9ea15914934f4bdcd80
Horia Coman <horia141@gmail.com>
2017-11-28 18:29:58 +0200

    Add another batch of tests for auth0-auth-flow-router

--- src/server/auth0-auth-flow-router-test.ts
+++ src/server/auth0-auth-flow-router-test.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import 'mocha'
 
 import {
+    Auth0AuthorizationCodeMarshaller,
     Auth0AccessTokenMarshaller
 } from './auth0-auth-flow-router'
 
@@ -54,6 +55,62 @@ describe('Auth0AccessTokenMarshaller', () => {
         for (let accessToken of auth0AccessTokens) {
             it(`should produce the same input "${accessToken}"`, () => {
                 const marshaller = new Auth0AccessTokenMarshaller();
+
+                expect(marshaller.pack(accessToken)).to.eql(accessToken);
+            });
+        }
+    });
+});
+
+
+describe('Auth0AuthorizationCodeMarshaller', () => {
+    const auth0AuthorizationCode = [
+        'foogazi',
+        'aGoodToken',
+        'A-Good-Token',
+        'A_Good_Token'
+    ];
+
+    const emptyAuth0AuthorizationCodes = [
+        ''
+    ];
+
+    const badAuth0AuthorizationCodes = [
+        '$',
+        '  ',
+        'a.bad.token'
+    ];
+
+    describe('extract', () => {
+        for (let accessToken of auth0AuthorizationCode) {
+            it(`should extract "${accessToken}"`, () => {
+                const marshaller = new Auth0AuthorizationCodeMarshaller();
+
+                expect(marshaller.extract(accessToken)).to.eql(accessToken);
+            });
+        }
+
+        for (let accessToken of emptyAuth0AuthorizationCodes) {
+            it(`should throw for empty "${accessToken}"`, () => {
+                const marshaller = new Auth0AuthorizationCodeMarshaller();
+
+                expect(() => marshaller.extract(accessToken)).to.throw('Expected a string to be non-empty');
+            });
+        }
+
+        for (let accessToken of badAuth0AuthorizationCodes) {
+            it(`should throw for bad "${accessToken}"`, () => {
+                const marshaller = new Auth0AuthorizationCodeMarshaller();
+
+                expect(() => marshaller.extract(accessToken)).to.throw('Should only contain alphanumerics');
+            });
+        }
+    });
+
+    describe('pack', () => {
+        for (let accessToken of auth0AuthorizationCode) {
+            it(`should produce the same input "${accessToken}"`, () => {
+                const marshaller = new Auth0AuthorizationCodeMarshaller();
 
                 expect(marshaller.pack(accessToken)).to.eql(accessToken);
             });
